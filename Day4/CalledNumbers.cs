2f359fa942715d78f5824995cb22e0cfa1cd4d0b
DoctorHowser <doctorhowser@gmail.com>
2021-12-10 21:10:53 -0600

    comment bingo

--- Day4/CalledNumbers.cs
+++ Day4/CalledNumbers.cs
@@ -3,17 +3,20 @@ namespace Advent;
 class CalledNumbers
 {
     //Read the file, split at ,
+    //Works fine, I like . notation for LINQ better
     public static List<int> nums = (
         File.ReadAllText(@"./data/bingoNumbers.txt")
         .Split(',').Select(Int32.Parse).ToList());
 
     //Read the file, split at new line x2 which should be for empty line...
-    //Transform each segment into <List<List<int>>
-    //Make BingoCard for each segment?
+    //Transform each segment into <List<List<int>> for BingoCard
+    //Feels awfully messy. Its probably a result of my 2d List data structure.
+    //I bet if I did this as some sort of dictionary of lists it would be more...sane?
+    // 3x ToList()
     public static List<BingoCard> cards = (
             File.ReadAllText(@"./data/bingoCards.txt")
             .Split(Environment.NewLine + Environment.NewLine)
-            .Select(card =>{
+            .Select(card => {
                 //Console.WriteLine(card);
                 return new BingoCard(card.Split(Environment.NewLine)
                     .Select(line =>
